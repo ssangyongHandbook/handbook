6c551b742c28a14ef403676d032d5d4d52e76068
jongsoo <seyeonpark12@naver.com>
2023-05-03 12:25:10 +0900

    scroll

--- src/main/java/com/sns/handbook/mapper/CommentMapperInter.java
+++ src/main/java/com/sns/handbook/mapper/CommentMapperInter.java
@@ -1,8 +1,18 @@
 package com.sns.handbook.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.sns.handbook.dto.CommentDto;
 
 @Mapper
 public interface CommentMapperInter {
 
+	public int getMaxNum();
+	public void updateStep(Map<String, Integer> map);
+	public void insert(CommentDto dto);
+	public void delete(String comment_num);
+	public CommentDto getData(String comment_num);
+	public void update(CommentDto dto);
 }
--- src/main/java/com/sns/handbook/serivce/CommentServiceInter.java
+++ src/main/java/com/sns/handbook/serivce/CommentServiceInter.java
@@ -1,5 +1,15 @@
 package com.sns.handbook.serivce;
 
+
+
+import com.sns.handbook.dto.CommentDto;
+
 public interface CommentServiceInter {
 
+	public int getMaxNum();
+	public void updateStep(int comment_group,int comment_step);
+	public void insert(CommentDto dto);
+	public void delete(String comment_num);
+	public CommentDto getData(String comment_num);
+	public void update(CommentDto dto);
 }
