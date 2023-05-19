a7bebe476bafa53219074377758017a3e136bd90
experthun <kimkt377@naver.com>
2023-05-19 23:00:14 +0900

    0519 commit banghun

--- src/main/java/com/sns/handbook/mapper/BookmarkMapperInter.java
+++ src/main/java/com/sns/handbook/mapper/BookmarkMapperInter.java
@@ -1,8 +1,16 @@
 package com.sns.handbook.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.sns.handbook.dto.BookmarkDto;
 
 @Mapper
 public interface BookmarkMapperInter {
-
+	
+	public void insertBookmark(BookmarkDto dto);
+	public void deleteBookmark(Map<String, String> map);
+	public int bookmarkCheck(Map<String, String> map);
+	
 }
